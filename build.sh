99782d79b0bd02205dd28ad6f26f899b4a03c75b
Akhil Narang <akhilnarang.1999@gmail.com>
2015-05-30 00:48:44 +0530

    Tweak the build script!
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
  #
- # Copyright © 2015, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
+ # Copyright © 2015, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>
  #
- # Custom build script
- # Modified by Akhil Narang for OwnKernel
+ # Custom build script for OwnKernel
  #
  # This software is licensed under the terms of the GNU General Public
  # License version 2, as published by the Free Software Foundation, and
@@ -15,9 +14,10 @@
  #
  # Please maintain this if you use this script or any part of it
  #
-KERNEL_DIR=$PWD
-ZIMAGE=$KERNEL_DIR/arch/arm/boot/zImage
-BUILD_START=$(date +"%s")
+./colours
+dir=$PWD
+op=$dir/arch/arm/boot/zImage
+START=$(date +"%s")
 blue='\033[0;34m'
 cyan='\033[0;36m'
 yellow='\033[0;33m'
@@ -28,31 +28,41 @@ export CROSS_COMPILE="/home/akhil/android/arm-cortex_a7-linux-gnueabihf-linaro_4
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER="akhilnarang"
-export KBUILD_BUILD_HOST="Insomniac"
+export KBUILD_BUILD_HOST="Give-This-Sleepless-Laptop-A-Break"
 
 compile_kernel ()
 {
-echo -e "$blue***********************************************"
+echo -e "$bldblu***********************************************"
 echo "          Compiling OwnKernel          "
 echo -e "***********************************************$nocol"
 make sprout_defconfig
 make -j12
-if ! [ -a $ZIMAGE ];
+if [ ! -e $op ]
 then
-echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
-exit 1
+echo -e "$bldred Kernel Compilation failed! Fix the errors! $nocol"
+exit
 fi
 }
 
 case $1 in
 clean)
-make ARCH=arm -j8 clean mrproper
-rm -rf include/linux/autoconf.h
+make clean
+rm -f include/linux/autoconf.h
 ;;
+mrproper)
+make mrproper
+;;
+menu|menuconfig)
+make sprout_defconfig menuconfig
+;;
+cleanbuild)
+make clean mrproper
+rm -f include/linux/autoconf.h
+compile_kernel
 *)
 compile_kernel
 ;;
 esac
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+END=$(date +"%s")
+DIFF=$(($END - $START))
+echo -e "$bldcya OwnKernel Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
