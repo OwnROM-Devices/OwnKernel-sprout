98c4827b040734a3d660bb118d2efb450d008004
W4TCH0UT <ateekujjawal@gmail.com>
2015-07-28 20:15:23 +0530

    Sorry, you did nothing
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- arch/arm/hotplug/blu_plug.c
+++ arch/arm/hotplug/blu_plug.c
@@ -5,8 +5,6 @@
  *
  * Copyright (C) 2015 engstk <eng.stk@sapo.pt> (hammerhead port, fixes and changes to blu_plug) 
  *
- * Copyright (C) 2015 Swapnil Solanki <swapnil133609@gmail.com> (Sprout port)
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
